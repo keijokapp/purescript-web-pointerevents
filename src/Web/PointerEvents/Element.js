a4d02ba597e971423df60acdb52666c6d3cce150
Keijo Kapp <keijo.kapp@gmail.com>
2023-02-17 00:47:48 +0200

    Reordered parameters to match conventional object-last ordering

--- src/Web/PointerEvents/Element.js
+++ src/Web/PointerEvents/Element.js
@@ -1,23 +1,23 @@
 
 
-export function setPointerCapture(element) {
-  return function (pointerId) {
+export function setPointerCapture(pointerId) {
+  return function (element) {
     return function () {
       return element.setPointerCapture(pointerId);
     };
   };
 }
 
-export function releasePointerCapture(element) {
-  return function (pointerId) {
+export function releasePointerCapture(pointerId) {
+  return function (element) {
     return function () {
       return element.releasePointerCapture(pointerId);
     };
   };
 }
 
-export function hasPointerCapture(element) {
-  return function (pointerId) {
+export function hasPointerCapture(pointerId) {
+  return function (element) {
     return function () {
       return element.hasPointerCapture(pointerId);
     };
--- src/Web/PointerEvents/Element.purs
+++ src/Web/PointerEvents/Element.purs
@@ -4,6 +4,6 @@ import Prelude (Unit)
 import Effect (Effect)
 import Web.DOM.Element (Element)
 
-foreign import setPointerCapture :: Element -> Int -> Effect Unit
-foreign import releasePointerCapture :: Element -> Int -> Effect Unit
-foreign import hasPointerCapture :: Element -> Int -> Effect Boolean
+foreign import setPointerCapture :: Int -> Element -> Effect Unit
+foreign import releasePointerCapture :: Int -> Element -> Effect Unit
+foreign import hasPointerCapture :: Int -> Element -> Effect Boolean
